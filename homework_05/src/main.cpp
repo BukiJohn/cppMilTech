4b56349e2bf271426a947ccfed6f12c70b502dd2
Buchmei Ivan <ivan.b@playliner.com>
2026-05-22 19:08:27 +0000

    Update: Sync with core project

--- homework_05/src/main.cpp
+++ homework_05/src/main.cpp
@@ -10,10 +10,14 @@ int main(int argc, char** argv) {
     }
 
     Frame frames[MAX_TELEMETRY_FRAMES];
-    const int frame_count = read_frames(argv[1], frames, MAX_TELEMETRY_FRAMES);
+    int frames_total = 0;
+    const int frames_valid = read_frames(argv[1], frames, MAX_TELEMETRY_FRAMES, frames_total);
 
-    const Summary summary = summarize(frames, frame_count);
+    const Summary summary = summarize(frames, frames_valid, frames_total);
     print_summary(summary);
 
+    if (frames_total == 0 || frames_valid != frames_total) {
+        return 1;
+    }
     return 0;
 }
